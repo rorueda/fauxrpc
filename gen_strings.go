dc6e0190f280ca26d8a4baaba86a3cfd36741718
rrueda <rorueda@gmail.com>
2025-05-22 16:30:46 -0300

    fix: Remove duplicated if block and switch to non-deprecated method

--- gen_strings.go
+++ gen_strings.go
@@ -47,11 +47,7 @@ func String(fd protoreflect.FieldDescriptor, opts GenOptions) string {
 	if constraints == nil {
 		return stringSimple(fd, opts)
 	}
-	rules := constraints.GetString_()
-	if rules == nil {
-		return stringSimple(fd, opts)
-	}
-
+	rules := constraints.GetString()
 	if rules == nil {
 		return stringSimple(fd, opts)
 	}
